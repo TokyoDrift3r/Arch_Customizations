04f333166ecfe406f37f5aa41286de9ded838a33
TokyoDrift3r <99697946+TokyoDrift3r@users.noreply.github.com>
2022-02-16 23:20:06 +0000

    Update 8-software-aur.sh

--- 8-software-aur.sh
+++ 8-software-aur.sh
@@ -19,47 +19,47 @@ PKGS=(
 
     # SYSTEM UTILITIES ----------------------------------------------------
 
-    'menulibre'                 # Menu editor
+    #'menulibre'                 # Menu editor
     'gtkhash'                   # Checksum verifier
 
     # TERMINAL UTILITIES --------------------------------------------------
 
-    'hyper'                     # Terminal emulator built on Electron
+    #'hyper'                     # Terminal emulator built on Electron
 
     # UTILITIES -----------------------------------------------------------
 
     'dropbox'                   # Cloud file storage
-    'enpass-bin'                # Password manager
-    'slimlock'                  # Screen locker
-    'oomox'                     # Theme editor
+    #'enpass-bin'                # Password manager
+    #'slimlock'                  # Screen locker
+    #'oomox'                     # Theme editor
 
     # DEVELOPMENT ---------------------------------------------------------
     
-    'visual-studio-code-bin'    # Kickass text editor
+    #'visual-studio-code-bin'    # Kickass text editor
 
     # MEDIA ---------------------------------------------------------------
 
     'spotify'                   # Music player
-    'screenkey'                 # Screencast your keypresses
+    #'screenkey'                 # Screencast your keypresses
     # 'aftershotpro3'             # Photo editor
 
     # POST PRODUCTION -----------------------------------------------------
 
-    'peek'                      # GIF animation screen recorder
+    #'peek'                      # GIF animation screen recorder
 
     # COMMUNICATIONS ------------------------------------------------------
 
-    'skypeforlinux-stable-bin'  # Skype
+    #'skypeforlinux-stable-bin'  # Skype
 
     # THEMES --------------------------------------------------------------
 
-    'gtk-theme-arc-git'
-    'adapta-gtk-theme-git'
-    'paper-icon-theme'
-    'tango-icon-theme'
-    'tango-icon-theme-extras'
-    'numix-icon-theme-git'
-    'sardi-icons'
+    #'gtk-theme-arc-git'
+    #'adapta-gtk-theme-git'
+    #'paper-icon-theme'
+    #'tango-icon-theme'
+    #'tango-icon-theme-extras'
+    #'numix-icon-theme-git'
+    #'sardi-icons'
 )
 
 
@@ -67,7 +67,7 @@ cd ${HOME}/AURIC
 chmod +x auric.sh
 
 for PKG in "${PKGS[@]}"; do
-    ./auric.sh -i $PKG
+    sudo yay -S "$PKG" --noconfirm --needed
 done
 
 echo
